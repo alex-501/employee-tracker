f267ebd17c4bba2422f69e4abc39a5ad7dbdf493
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-04-24 14:51:39 -0700

    sqlite3 db/election.db < db/schema.sql
    sqlite3 db/election.db < db/schema.sql worked without errors but there are unrecognized characters

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,10 @@
 CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  Last_name VARCHAR(3O) NOT NULL,
+  Last_name VARCHAR(30) NOT NULL,
    party_id INTEGER UNSIGNED,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
-
-
-
-CREATE TABLE role (
-  id INTEGER PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary VARCHAR(30) NOT NULL,
-  department VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
 );
 
 
@@ -36,11 +22,25 @@ VALUES
   ('Unica', 'Zurn', NULL, 1);
 
 
+
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL
+);
+
   insert into role(title, salary, department) values('Manager', 200000, 1);
 insert into role(title, salary, department) values('Engineer', 220000,2 );
 insert into role(title, salary, department) values('Accountant', 240000, 3);
 insert into role(title, salary, department) values('Administration', 20000, 4);
 
+
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
 
 insert into department(name)
