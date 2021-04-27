d4f7576237d4b7ce3ddaf76c4ed9966ed38e6a73
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-04-26 20:04:42 -0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -11,7 +11,7 @@ const  connection = mysql.createConnection({
 //how to connect db 
 
 
-let sql = 'CREATE TABLE employees ( id INT(11) NOT NULL AUTO_INCREMENT, first_name VARCHAR(30) NOT NULL, last_name VARCHAR(30) NOT NULL, role_id INT, manager_id INT, PRIMARY KEY (id),CONSTRAINT fk_manager  FOREIGN KEY (manager_id) REFERENCES employees(id) ON UPDATE CASCADE   ON DELETE SET NULL, CONSTRAINT fk_role  FOREIGN KEY (role_id)  REFERENCES roles(id) ON UPDATE CASCADE ON DELETE SET NULL);';
+let sql = 'INSERT INTO departments (id, name) VALUES (  1 , Supervisor),( 2 , HR),( 3 , accounting),( 4 , Mental Health);';
 connection.query(sql,(err,result)=>{
 if (err) throw err
 console.log('database created');})
